00597f86d3108b16ad96ef5ed53b9eb3ad79d5b5
SrVladitor13 <cesar.broggi@gmail.com>
2022-08-18 11:43:32 -0300

    Aniadi un archivo extra

--- Java/src/TPNro4.java
+++ Java/src/TPNro4.java
@@ -1,3 +1,5 @@
+import javafx.util.converter.IntegerStringConverter;
+
 import javax.swing.*;
 
 public class TPNro4{
@@ -9,9 +11,24 @@ public class TPNro4{
 
         JOptionPane.showMessageDialog(null,"El resultado de " + num1 + " + " + num2 + " es: " + resultado);
 
-        if 
+        if (resultado > 100) {
+            JOptionPane.showMessageDialog(null, "Es mayor a 100 porque es: " + resultado);
+        }
+        else if (resultado == 100) {
+            JOptionPane.showMessageDialog(null, "100");
+        }
+        else if (resultado < 0) {
+            JOptionPane.showMessageDialog(null, "El resultado es negativo");
+        }
+
+        int listaDeNumeros [] = {};
+        String numeroIngresado = null;
+
+        while(!numeroIngresado.equals("f")){
+            int contador;
+            numeroIngresado= IntegerStringConverter.parseInt(JOptionPane.showInputDialog("Ingrese un numero (0 para detener el programa"));
+            listaDeNumeros
+        }
+
     }
 }
-
-//ingresar 2 num, sumar y printear "a + b = "
-

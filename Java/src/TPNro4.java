546d3036048badc3c36516892dd3f8a2998fb616
Shizukku <cesar.broggi@gmail.com>
2022-08-18 10:52:10 -0300

    Agrego el package de mi projecto de Java

---
+++ Java/src/TPNro4.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class TPNro4{
+    public static void main(String[] args){
+        int num1=Integer.parseInt(JOptionPane.showInputDialog("Introduce el primer numero"));
+        int num2=Integer.parseInt(JOptionPane.showInputDialog("Introduce el segundo numero"));
+
+        int resultado = num1 + num2;
+
+        JOptionPane.showMessageDialog(null,"El resultado de " + num1 + " + " + num2 + " es: " + resultado);
+
+        if 
+    }
+}
+
+//ingresar 2 num, sumar y printear "a + b = "
+

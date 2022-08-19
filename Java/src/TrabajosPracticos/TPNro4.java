74c541bf403aef0e6ec210bd17ca225cf6950d15
Lucas Luque <lucasluqueencina@gmail.com>
2022-08-19 11:51:16 -0300

    Correccion de un error en TPNro4.java

--- Java/src/TrabajosPracticos/TPNro4.java
+++ Java/src/TrabajosPracticos/TPNro4.java
@@ -1,15 +1,12 @@
-//Me di cuenta de que solamente había que declarar las variables después de terminar todo el código
-//así que aprovecho a testear si usted considera que excederse o hacer cosas que no pidió explicitamente
-//se consideran mal
-
 package TrabajosPracticos;
 import javax.swing.*;
 
 public class TPNro4{
     public static void main(String[] args){
-        String nombre=JOptionPane.showInputDialog(null, "Ingrese su nombre");
-        int edad=Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese su edad"));
-        float salario=Float.parseFloat(JOptionPane.showInputDialog(null, "Ingrese el salario que le gustaría ganar mensualmente (con centavos)"));
+        String nombre="Lucas Lionel Luque Encina";
+        int edad=21;
+        float salario=999999.5f; 
+        //Euros
         boolean carnet= true;
 
         JOptionPane.showMessageDialog(null, "Nombre: " + nombre + "\nEdad: " + edad + "\nSalario deseado: " + salario + "\nCarnet: " + carnet);
